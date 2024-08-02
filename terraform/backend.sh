333e0fd6d4dbee71ad64d7292ef6df8eac5b1084
puneeth tiruveedula <tiruveedulapuneeth3102@gmail.com>
2024-08-02 15:03:53 +0530

    pipeline

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -4,4 +4,4 @@ environment=$2
 appVersion=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/puneethtiruveedula/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appversion=$appVersion
+ansible-pull -i localhost, -U https://github.com/puneethtiruveedula/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$appVersion
